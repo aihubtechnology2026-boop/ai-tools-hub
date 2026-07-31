ea0788d4140a1137b319a29f4d5c810b505232ee
aihubtechnology2026-boop <aihubtechnology2026@gmail.com>
2026-08-01 02:34:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const searchInput = document.getElementById("search");
+const cards = document.querySelectorAll(".card");
 
+searchInput.addEventListener("keyup", function () {
+    const value = searchInput.value.toLowerCase();
+
+    cards.forEach(card => {
+        const text = card.textContent.toLowerCase();
+
+        if (text.includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
